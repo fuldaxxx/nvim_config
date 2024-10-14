4264ee9ba30991a3364e498dafc2136781d32de4
Ruslan Gorbunov <ruslangorbunovv@gmail.com>
2024-10-14 19:33:46 +0300

    fix tabs

--- lua/options.lua
+++ lua/options.lua
@@ -3,5 +3,9 @@ require "nvchad.options"
 -- add yours here!
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.tabstop = 8
+vim.o.softtabstop = 8
+vim.o.shiftwidth = 8
+vim.o.expandtab = false
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
